8e97002f786cdc0e69739a1802fc01ec4497d4a5
Greg Gomez <greg.gomez.mil@a@usmc.mil>
2025-02-27 08:48:30 -0600

    notes in arrays and worked on Lab1

--- JAVASCRIPT/Arrays/script.js
+++ JAVASCRIPT/Arrays/script.js
@@ -1,38 +1,72 @@
-let numbers = [1, 2, 3]
+// let numbers = [1, 2, 3]
 //make a copy and use it to manipulate the data then reassign to numbers
-let data = numbers.slice()
+// let data = numbers.slice()
 // append to the array
 //push()
-numbers.push(4)
-console.log(numbers);
+// numbers.push(4)
+// console.log(numbers);
 //pop removed the last element
-let removedElement = numbers.pop()
-console.log(removedElement);
+// let removedElement = numbers.pop()
+// console.log(removedElement);
 
 //front of the array - prepending
-numbers.unshift(100)
-console.log(numbers);
-let newResult = numbers.shift()
-console.log(newResult);
+// numbers.unshift(100)
+// console.log(numbers);
+// let newResult = numbers.shift()
+// console.log(newResult);
 
 //once perfect - reassign it back to numbers
-numbers = data
+// numbers = data
 
 // more of a string method
 // .trim
 // "                      Cruz is "
-let message = "                      Cruz is "
-console.log (message.trim());
+// let message = "                      Cruz is "
+// console.log (message.trim());
 
-let students = ["ALice", "Bob", "Charlie"];
-console.log(students)
-students.push("Diana")
-console.log(students);
-let removeLast = students.pop()
-console.log(removeLast);
-students.unshift("Eve")
-console.log(students);
-let newFirst = students.unshift()
-console.log(newFirst);
-let removeFirst = students.shift()
-console.log(removeFirst);
+// let students = ["ALice", "Bob", "Charlie"];
+// console.log(students)
+// students.push("Diana")
+// console.log(students);
+// let removeLast = students.pop()
+// console.log(removeLast);
+// students.unshift("Eve")
+// console.log(students);
+// let newFirst = students.unshift()
+// console.log(newFirst);
+// let removeFirst = students.shift()
+// console.log(removeFirst);
+
+//foreach()
+// let numbers = [4, 6, 8]
+// //predicate = name used to reference each element
+// numbers.forEach(function(element){
+//     console.log(element);
+// })
+
+// let names = ["JM", "Jeremiah", "Chris"]
+// names.forEach(el =>{
+//     console.log(`${el}'s`);
+// })
+
+// //.filter()
+// let newNumbers = [2, 3, 4]
+// let newArray = newNumbers.filter(function (displayEven){
+//     return displayEven %2 === 0 //should be even only -- returns the element that passes
+// })
+// console.log(newArray)
+
+//.sort -- strings only
+// let fruits = ["Dragon fruit", "Peach", "Apple"]
+// //fruits.sort()
+// let newFruits = fruits.map(el =>{
+//     return el.toLocaleLowerCase()
+// })
+// console.log(newFruits.sort());
+
+// let numbers = [10, 1, 100, 3, 34, 11]
+// numbers.sort(function (a,b){
+//     //return a-b // ascending
+//     return b-a // descending
+// })
+// console.log(numbers);
--- Labs/Lab1/script.js
+++ Labs/Lab1/script.js
@@ -9,10 +9,10 @@
 // Based on their input, determine how many years old they are.
 // Log the calculated age to the console: “Your age is:”
 
-let birthdate = prompt ("What year were you born?")
-let x = 2025
-let age = x - parseInt(birthdate)
- alert ("You are " + age + " years old !") ;
+// let birthdate = prompt ("What year were you born?")
+// let x = 2025
+// let age = x - parseInt(birthdate)
+// console.log ("You are " + age + " years old !") ;
 
 // 2. Simple Interest Calculator
 // Use a traditional function named calculateSimpleInterest to calculate the simple interest using the formula:
@@ -21,8 +21,26 @@ let age = x - parseInt(birthdate)
 //     Store each of these in a separate variable
 // Log the result to the console: “Your simple interest is:”
 
-let Principal = prompt ("What is the principal amount?")
-let Rate = prompt ("What is the rate of interest?")
-let Time = prompt ("What is the time in years?")
-let calculateSimpleInterest = (Principal * Rate * Time)/100
-console.log("Your simple interest is" + calculateSimpleInterest);
+// let Principal = prompt ("What is the principal amount?")
+// let Rate = prompt ("What is the rate of interest?")
+// let Time = prompt ("What is the time in years?")
+// let calculateSimpleInterest = (Principal * Rate * Time)/100
+// console.log("Your simple interest is: " + calculateSimpleInterest);
+
+// 3. Favorite Color Selector
+// Create an array with three colors.
+// Use a traditional function named addColor to add a new color to an array.
+// Takes one argument - the array of colors
+// Prompt the user to input a color name to add to the array.
+// Prepend the users color to the existing array
+// Log the updated array to the console using: “Updated colors: “
+
+let arrayOfColors = ["Green", "Purple", "Pink"];
+function addColors(arrayAsParameter) {
+    arrayAsParameter.unshift("Red")
+}
+addColors(arrayOfColors);
+console.log(arrayOfColors);
+let userInput = prompt ("Please choose a color to add")
+arrayOfColors.unshift()
+console.log(userInput + arrayOfColors)
